e9ea9d518a276eadec98adf609ec014b036ce96f
cYpHeR <64411912+cypher-nullbyte@users.noreply.github.com>
2021-06-01 21:49:18 +0530

    Multiple Inheritance Amazing Session

--- C++/Core/Inheritance/multiple_inheritance.cpp
+++ C++/Core/Inheritance/multiple_inheritance.cpp
@@ -1,1 +1,72 @@
-//  https://www.youtube.com/watch?v=-902HO2DO5I
+#include<iostream>
+
+
+class Youtuber  {
+    private:
+        int xy;
+    public:
+        Youtuber(int x) : xy{x}
+        {
+            std::cout<<"YouTuber "<<x<<"\n";
+        }
+        void work()
+        {
+            std::cout<<"Working as YouTuber\n";
+        }
+        void getX()
+        {
+            std::cout<<xy<<"\n";
+        }
+
+};
+class Engineer {
+    private:
+        int xe;
+    public:
+        Engineer(int x) : xe{x}
+        {
+            std::cout<<"Engineer "<<x<<"\n";
+        }
+        void work()
+        {
+            std::cout<<"Working as Engineer\n";
+        }
+        void getX()
+        {
+            std::cout<<xe<<"\n";
+        }
+};
+class cYpHeR: public Youtuber, public Engineer // order of constructor same
+{
+
+    public:
+        cYpHeR(int x, int y) :Engineer(x), Youtuber(y) // has no influence on order. Order is as in class inheritence order
+        {
+            std::cout<<"cYpHeR "<<x<<" "<<y<<"\n";
+        }
+        void work()
+        {
+            std::cout<<"Work as Both :)\n";
+            Engineer::work();
+            Youtuber::work();
+        }
+        
+};
+
+
+
+int main()
+{
+    cYpHeR c(2,4);
+    std::cout<<"-------------------------------------------------\n";
+    c.Engineer::work();
+    c.Youtuber::work();
+    std::cout<<"-------------------------------------------------\n";
+    c.work();
+    std::cout<<"----------------Let us do Slicing---------------\n";
+    // slicing is simple taking Engineer  part of object c.
+    Engineer en=c;
+    en.work();
+    en.getX();
+    return 0;
+}
